7a3cd8e47fe865bbd77054d0a785316df861b31f
Abigail Plata <abigailplataa@gmail.com>
2022-03-11 21:03:59 -0800

    linking pages together w python

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html',my_name='Abigail Plata')
+
+@app.route('/resume', methods=['GET','POST'])
+def resume():
+    if request.method == 'POST':
+        # can do stuff when the form is submitted
+        #redirect to end the POST handling
+        #the redirect can be to the same route or elsewhere
+        return redirect(url_for('index'))
+    #show the form, it wasnt submitted
+    return render_template('resume.html')
